73e8e292894708da3b43d1a99973ec2f373160fa
Artur Abdalimov <a.abdalimov.97@gmail.com>
2018-02-06 21:51:09 +0600

    Added Transaction

--- ShopLibrary/ShopLibrary/Models/Order.cs
+++ ShopLibrary/ShopLibrary/Models/Order.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ShoppingLibrary.Models
 {
     public class Order
     {
+        public Transaction Transaction { get; set; }
         private List<Tuple<int, int>> products;
 
         public Order()
@@ -20,6 +22,14 @@ namespace ShoppingLibrary.Models
         public List<Tuple<int, int>> GetProducts()
         {
             return products;
+        }
+
+        public Transaction PrepareTransaction(List<Product> products)
+        {
+            var sum = products.Sum(x => x.price);
+            var t = new Transaction(new Random(1000).Next(), sum);
+            this.Transaction = t;
+            return t;
         }
     }
 }
--- ShopLibrary/ShopLibrary/Models/Transactions.cs
+++ ShopLibrary/ShopLibrary/Models/Transactions.cs
@@ -1,7 +1,20 @@
-﻿namespace ShoppingLibrary.Models
+﻿using System;
+using System.Data.Common;
+using Microsoft.Win32.SafeHandles;
+
+namespace ShoppingLibrary.Models
 {
-    public class Transactions
+    public class Transaction
     {
-        
+        public int Id { get; set; }
+        public float Charge { get; set; }
+        public DateTime TransactionDateTime { get; set; }
+
+        public Transaction(int id, float charge)
+        {
+            this.Id = id;
+            this.Charge = charge;
+            this.TransactionDateTime = DateTime.Now;
+        }
     }
 }
